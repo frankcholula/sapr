37100b4b731a194647fb96d14492e4e96d6bf298
frankcholula <lu.phrank@gmail.com>
2024-10-30 09:23:23 +0000

    refactor.

--- assignment_1/playAudio.m
+++ assignment_1/playAudio.m
@@ -5,22 +5,16 @@ function [audio, fs, duration, numSamples] = playAudio(fileName)
     numSamples = length(audio);
     fprintf('Playing %s at %d Hz with duration %.2f seconds and %d samples\n',fileName, fs, duration, numSamples);
     
-    % plot the spectrogram
-    figure;
+    figure();
     subplot(3,1,1);
-    spectrogram(audio, hamming(960), 240, 1024, fs, 'yaxis');
-    title(['Spectrogram of ', fileName]);
-    
-    % plot the waveform
+    plotSpectrogram(audio, fs, fileName);
     subplot(3,1,2);
-    time = (0:numSamples - 1) / fs;
-    plot(time, audio);
-    title(['Waveform of ', fileName])
-    sound(audio, fs);
-    pause(duration + 1)
-
-    % plot the spectrum as well
+    plotWaveform(audio, fs, fileName, numSamples, duration)
     subplot(3, 1, 3);
+    plotPowerSpectrum(audio, fs, fileName, numSamples);
+end
+
+function plotPowerSpectrum(audio, fs, fileName, numSamples)
     audio_fft = fft(audio);
     power_spectrum = abs(audio_fft(1:floor(numSamples / 2) + 1)).^2;
     freq = (0:floor(numSamples / 2)) * (fs / numSamples);
@@ -29,4 +23,19 @@ function [audio, fs, duration, numSamples] = playAudio(fileName)
     xlabel('Frequency (Hz)');
     ylabel('Power (dB)');
     xlim([0 300]);
+end
+
+function plotWaveform(audio, fs, fileName, numSamples, duration)
+    time = (0:numSamples - 1) / fs;
+    plot(time, audio);
+    title(['Waveform of ', fileName])
+    xlabel('Time (s)');
+    ylabel('Amplitude');
+    sound(audio, fs);
+    pause(duration + 1)
+end
+
+function plotSpectrogram (audio, fs, fileName)
+    spectrogram(audio, hamming(960), 240, 1024, fs, 'yaxis');
+    title(['Spectrogram of ', fileName]);
 end
